aab62d2b82840d70d5d4327a568627950a095263
Chuck Findlay <chuck@findlayis.me>
2024-01-07 18:09:42 -0400

    pointer so ColourCode defaults as null when sent out

--- model/model.go
+++ model/model.go
@@ -3,7 +3,7 @@ package model
 type WebhookMessage struct {
 	Message    string
 	Password   string
-	ColourCode int8 // https://modern.ircdocs.horse/formatting.html
+	ColourCode *int8 // https://modern.ircdocs.horse/formatting.html
 }
 
 type DirectedWebhookMessage struct {
