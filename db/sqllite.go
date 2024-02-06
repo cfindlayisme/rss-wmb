bf793bdef9d3fa2a90c1944c98baad9872024e57
Chuck Findlay <chuck@findlayis.me>
2024-02-05 20:50:50 -0400

    remove some excess comments, daily cleanup of db items

--- db/sqllite.go
+++ db/sqllite.go
@@ -10,21 +10,18 @@ import (
 )
 
 func GetIfLinkPrintedInDB(link string) bool {
-	// Open the SQLite database
 	db, err := sql.Open("sqlite3", env.GetStateFilePath())
 	if err != nil {
 		return false
 	}
 	defer db.Close()
 
-	// Query the database
 	rows, err := db.Query("SELECT Link FROM FeedItems WHERE Link = ? AND Printed = 1", link)
 	if err != nil {
 		return false
 	}
 	defer rows.Close()
 
-	// Iterate over the rows
 	for rows.Next() {
 		var link string
 		err = rows.Scan(&link)
@@ -37,34 +34,64 @@ func GetIfLinkPrintedInDB(link string) bool {
 	return false
 }
 
-func WriteFeedItemsToDB(feedItemsNew map[string]bool) {
-	// Open the SQLite database
+func CleanDB() {
 	db, err := sql.Open("sqlite3", env.GetStateFilePath())
 	if err != nil {
 		log.Fatalf("Error opening SQLite database: %v", err)
 	}
 	defer db.Close()
 
-	// Create the table if it doesn't exist
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS FeedItems (Link TEXT PRIMARY KEY, Printed BOOLEAN, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")
-	if err != nil {
-		log.Fatalf("Error creating table: %v", err)
-	}
-
-	// Begin a transaction
 	tx, err := db.Begin()
 	if err != nil {
 		log.Fatalf("Error beginning transaction: %v", err)
 	}
 
-	// Prepare the statement
+	stmt, err := tx.Prepare("DELETE FROM FeedItems WHERE Timestamp < datetime('now', '-7 days')")
+	if err != nil {
+		log.Fatalf("Error preparing statement: %v", err)
+	}
+
+	res, err := stmt.Exec()
+	if err != nil {
+		log.Fatalf("Error executing statement: %v", err)
+	}
+
+	rowCount, err := res.RowsAffected()
+	if err != nil {
+		log.Fatalf("Error getting rows affected: %v", err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatalf("Error committing transaction: %v", err)
+	}
+
+	log.Printf("Cleaned up %d items older than 7 days from the database", rowCount)
+}
+
+func WriteFeedItemsToDB(feedItemsNew map[string]bool) {
+	db, err := sql.Open("sqlite3", env.GetStateFilePath())
+	if err != nil {
+		log.Fatalf("Error opening SQLite database: %v", err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS FeedItems (Link TEXT PRIMARY KEY, Printed BOOLEAN, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)")
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatalf("Error beginning transaction: %v", err)
+	}
+
 	stmt, err := tx.Prepare("INSERT OR IGNORE INTO FeedItems (Link, Printed) VALUES (?, ?)")
 	if err != nil {
 		log.Fatalf("Error preparing statement: %v", err)
 	}
 	defer stmt.Close()
 
-	// Insert the feed items into the table
 	for link, printed := range feedItemsNew {
 		_, err = stmt.Exec(link, printed)
 		if err != nil {
@@ -72,13 +99,11 @@ func WriteFeedItemsToDB(feedItemsNew map[string]bool) {
 		}
 	}
 
-	// Commit the transaction
 	err = tx.Commit()
 	if err != nil {
 		log.Fatalf("Error committing transaction: %v", err)
 	}
 
-	// Print out the link of the feed items added
 	for link := range feedItemsNew {
 		log.Printf("Added %s to the database\n", link)
 	}
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/cfindlayisme/rss-wmb/db"
 	"github.com/cfindlayisme/rss-wmb/env"
 	"github.com/cfindlayisme/rss-wmb/rss"
 )
@@ -12,6 +15,13 @@ func main() {
 
 	rss.CheckFeeds(feedChannels, feedURLs)
 	rss.ScheduleFeeds(scheduledDuration, feedChannels, feedURLs)
+
+	// Once per day cleanup DB (also at start)
+	go func() {
+		for range time.Tick(24 * time.Hour) {
+			db.CleanDB()
+		}
+	}()
 
 	// Keep the main goroutine running
 	select {}
